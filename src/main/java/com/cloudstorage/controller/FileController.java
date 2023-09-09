65e8ce7a0a624469893c3aa130e94e6377c8e3e1
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-09-09 21:23:32 +0200

    feat(controller): add file, folder and search controllers

---
+++ src/main/java/com/cloudstorage/controller/FileController.java
@@ -0,0 +1,12 @@
+package com.cloudstorage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/files")
+@RequiredArgsConstructor
+public class FileController {
+
+}
