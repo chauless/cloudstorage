628a23826a5c5a6afaeb99f8c2518d2b63bb1c23
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-09-10 13:20:19 +0200

    feat(dto): add file upload/download dtos

---
+++ src/main/java/com/cloudstorage/dto/MinioObjectDto.java
@@ -0,0 +1,19 @@
+package com.cloudstorage.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class MinioObjectDto {
+
+    private String owner;
+
+    private Boolean isDir;
+
+    private String path;
+
+    private String name;
+}
