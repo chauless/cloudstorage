394d3f6198ed75976cdf302f492f7e2c27f5530f
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-09-10 21:35:49 +0200

    feat(controller): add uploadFile method

--- src/main/java/com/cloudstorage/controller/FileController.java
+++ src/main/java/com/cloudstorage/controller/FileController.java
@@ -1,7 +1,11 @@
 package com.cloudstorage.controller;
 
+import com.cloudstorage.dto.FileUploadRequest;
+import com.cloudstorage.service.FileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,4 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class FileController {
 
+    private final FileService fileService;
+
+    @PostMapping
+    public String uploadFile(@ModelAttribute("fileUploadRequest") FileUploadRequest fileUploadRequest) {
+        fileService.uploadFile(fileUploadRequest);
+        return "redirect:/";
+    }
 }
