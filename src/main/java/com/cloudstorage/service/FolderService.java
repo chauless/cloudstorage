a30c8811531fc40124cbb80f4049f1155a9a5093
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-09-13 17:17:36 +0200

    feat(service): add upload folders feature

---
+++ src/main/java/com/cloudstorage/service/FolderService.java
@@ -0,0 +1,55 @@
+package com.cloudstorage.service;
+
+import com.cloudstorage.config.MinioBucketConfig;
+import com.cloudstorage.dto.FolderUploadRequest;
+import io.minio.MinioClient;
+import io.minio.SnowballObject;
+import io.minio.UploadSnowballObjectsArgs;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.cloudstorage.utils.MinioRootFolderUtils.getUserRootFolderPrefix;
+
+@Service
+@RequiredArgsConstructor
+public class FolderService {
+
+    private final MinioClient minioClient;
+    private final MinioBucketConfig minioBucketConfig;
+
+    public void uploadFolder(FolderUploadRequest folderUploadRequest) {
+        List<MultipartFile> files = folderUploadRequest.getFiles();
+        String owner = folderUploadRequest.getOwner();
+        try {
+            List<SnowballObject> objects = convertToUploadObjects(files, owner);
+
+            minioClient.uploadSnowballObjects(UploadSnowballObjectsArgs.builder()
+                    .bucket(minioBucketConfig.getBucketName())
+                    .objects(objects)
+                    .build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<SnowballObject> convertToUploadObjects(List<MultipartFile> files, String owner) throws IOException {
+        List<SnowballObject> objects = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            SnowballObject snowballObject = new SnowballObject(
+                    getUserRootFolderPrefix(owner) + file.getOriginalFilename(),
+                    file.getInputStream(),
+                    file.getSize(),
+                    null
+            );
+            objects.add(snowballObject);
+        }
+
+        return objects;
+    }
+}
