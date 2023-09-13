62be198d116dee66abf54dcc551241436ec30f2f
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-09-13 17:15:33 +0200

    feat(security): files, folders and search can be reached by authorized users

--- src/main/java/com/cloudstorage/config/WebSecurityConfig.java
+++ src/main/java/com/cloudstorage/config/WebSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.cloudstorage.config;
 
 import com.cloudstorage.service.CustomUserDetailsService;
+import com.cloudstorage.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -20,11 +27,13 @@ import static com.cloudstorage.model.Role.ROLE_USER;
 public class WebSecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private UserService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/files", "/folders", "/search").hasAuthority(ROLE_USER.name())
                         .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
