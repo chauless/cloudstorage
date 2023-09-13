04a0e0b03a67095da802ef23ce83ecd824a330c1
Matvei Morenkov <morenmat@fel.cvut.cz>
2023-09-13 17:16:06 +0200

    feat(dto): add FolderUploadRequest

---
+++ src/main/java/com/cloudstorage/dto/FolderUploadRequest.java
@@ -0,0 +1,20 @@
+package com.cloudstorage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FolderUploadRequest {
+
+    private List<MultipartFile> files;
+
+    private String owner;
+}
